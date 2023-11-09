87b3a1e705cc3671a07a40a36f910c908c92d0f7
Kamil Hajduła <98982966+Lariw@users.noreply.github.com>
2023-11-10 00:05:03 +0100

    Added frontPage, some fixes with Electron.

---
+++ script/fronMenu.js
@@ -0,0 +1,10 @@
+const startButton = document.querySelector(".js-playButton");
+const canvaContainer = document.querySelector(".canvasContainer");
+const scoreContainer = document.querySelector(".scoreContainer");
+const frontPageContainer = document.querySelector(".frontPage");
+
+startButton.addEventListener("click", () => {
+  canvaContainer.style.display = "flex";
+  scoreContainer.style.display = "block";
+  frontPageContainer.style.display = "none";
+});
