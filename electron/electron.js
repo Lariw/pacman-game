b95419244e3d7428f6d4c79daab6c99b2e8099cc
Kamil Hajduła <98982966+Lariw@users.noreply.github.com>
2023-11-09 23:30:44 +0100

    Added electron and electron.js

---
+++ electron/electron.js
@@ -0,0 +1,54 @@
+const { app, BrowserWindow, ipcMain } = require("electron");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 700,
+    height: 800,
+    icon: "assets/icons/icon_256.ico",
+    frame: false,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+
+  win.loadFile("html/index.html");
+
+  ipcMain.on("closeApp", () => {
+    if (win) {
+      win.close();
+    }
+  });
+
+  ipcMain.on("minimizeApp", () => {
+    if (win) {
+      win.minimize();
+    }
+  });
+
+  ipcMain.on("maximizeRestoreApp", () => {
+    if (win) {
+      if (win.isMaximized()) {
+        win.restore();
+      } else {
+        win.maximize();
+      }
+    }
+  });
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
